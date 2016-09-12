9a72b10c853936e4a382272a164b05b62f03bdd2
Sarah Lehman <tug46038@temple.edu>
2016-09-12 16:27:28 -0400

    Successfully connected to GPS.  Now to force location updates so we can test...

--- app/src/main/java/edu/temple/indoorzonelocalizer/MainActivity.java
+++ app/src/main/java/edu/temple/indoorzonelocalizer/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.temple.indoorzonelocalizer;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,12 +8,22 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * Main activity class to display user's GPS coordinates
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static final String[] INITIAL_PERMS = {
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final int INITIAL_REQUEST = 1337;
+
+    protected LocationManager locManager;
+    TextView latLongTextView;
+
     /**
      *
      * @param savedInstanceState
@@ -22,65 +33,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        LocationListener mlocListener = new MyLocationListener();
+        latLongTextView=(TextView)findViewById(R.id.latLongTextView);
+        locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-        int locationAccess = getApplicationContext().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION");
-        if (locationAccess == PackageManager.PERMISSION_GRANTED) {
-            mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+        requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        if (PackageManager.PERMISSION_GRANTED == checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            latLongTextView.setText("Location permissions granted.  Launching localizer.");
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+        else {
+            latLongTextView.setText("Insufficient location permissions.");
         }
     }
 
     /**
      *
+     * @param loc
      */
-    public class MyLocationListener implements LocationListener {
-        /**
-         *
-         * @param loc
-         */
-        @Override
-        public void onLocationChanged(Location loc) {
-            loc.getLatitude();
-            loc.getLongitude();
-
-            String text = "My current location is: " +
+    @Override
+    public void onLocationChanged(Location loc) {
+        String text = "My current location is: " +
                 "Latitude = " + loc.getLatitude() +
                 ", Longitude = " + loc.getLongitude();
+        latLongTextView.setText(text);
+    }
 
-            Toast.makeText(getApplicationContext(),
-                text, Toast.LENGTH_SHORT).show();
-        }
+    /**
+     *
+     * @param provider
+     */
+    @Override
+    public void onProviderDisabled(String provider) {
+        latLongTextView.setText("GPS disabled.");
+    }
 
-        /**
-         *
-         * @param provider
-         */
-        @Override
-        public void onProviderDisabled(String provider) {
-            Toast.makeText(getApplicationContext(),
-                "GPS Disabled", Toast.LENGTH_SHORT ).show();
-        }
+    /**
+     *
+     * @param provider
+     */
+    @Override
+    public void onProviderEnabled(String provider) {
+        latLongTextView.setText("GPS enabled.");
+    }
 
-        /**
-         *
-         * @param provider
-         */
-        @Override
-        public void onProviderEnabled(String provider) {
-            Toast.makeText( getApplicationContext(),
-                "GPS Enabled", Toast.LENGTH_SHORT).show();
-        }
-
-        /**
-         *
-         * @param provider
-         * @param status
-         * @param extras
-         */
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            // do nothing for now
-        }
-    } /* End of class: MyLocationListener */
-} /* End of class: MainActivity */
+    /**
+     *
+     * @param provider
+     * @param status
+     * @param extras
+     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // do nothing for now
+    }
+}
